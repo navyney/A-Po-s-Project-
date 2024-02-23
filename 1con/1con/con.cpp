2f6cd7f70e95842c3cc64210519f1ea504cedc9e
PhichaPrim <152593599+PhichaPrim@users.noreply.github.com>
2024-02-23 20:27:37 +0700

    Delete 1con/1con directory

--- 1con/1con/con.cpp
+++
@@ -1,9 +0,0 @@
-#include "con.h"
-
-using namespace My1con;
-
-void main() {
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false);
-	Application::Run(gcnew con());
-}
