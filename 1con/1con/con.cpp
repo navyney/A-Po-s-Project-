162c45ad10adbfe95f1796a0ea8cc8eef1cea2ca
PhichaPrim <152593599+PhichaPrim@users.noreply.github.com>
2024-02-23 15:30:02 +0700

    Add files via upload

---
+++ 1con/1con/con.cpp
@@ -0,0 +1,9 @@
+#include "con.h"
+
+using namespace My1con;
+
+void main() {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew con());
+}
