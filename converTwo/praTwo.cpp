0c8e817d7c0225a7f7f36c6fb6b70aa41ab9d758
PhichaPrim <phichamon_duangmanee@cmu.ac.th>
2024-02-24 08:42:59 +0700

    con two

---
+++ converTwo/praTwo.cpp
@@ -0,0 +1,10 @@
+#include "praTwo.h"
+
+using namespace converTwo;
+
+void main() {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew praTwo());
+}
+
