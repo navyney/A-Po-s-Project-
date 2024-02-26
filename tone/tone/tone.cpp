5d5af228109ef33980a118b43e7fefb93982372e
Vualerie <jumper13577@gmail.com>
2024-02-26 22:41:39 +0700

    old tone

---
+++ tone/tone/tone.cpp
@@ -0,0 +1,9 @@
+#include "tones.h"
+
+using namespace tone;
+
+void main() {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    Application::Run(gcnew MyForm());
+}
